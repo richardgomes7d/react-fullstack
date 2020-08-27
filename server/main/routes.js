c9d07f04210cd5d7c24227dc2f998d2f7ee4fcd8
Ricardo David Antonetti <richardgomes7d@hotmail.com>
2020-08-27 18:09:42 -0500

    attempt to get ipInfo

--- server/main/routes.js
+++ server/main/routes.js
@@ -1,9 +1,12 @@
 var express = require('express')
-
+var expressip = require('express-ip')
 var router = express.Router()
 
+router.use(expressip().getIpInfoMiddleware);
+
 router.get('/hello', function(req, res) {
-    res.json('hello world')
+    const ipInfo = req.ipInfo;
+    res.json('hello world' + req.ip + ` Hey, you are browsing from ${ipInfo.city}, ${ipInfo.country}`)
 })
 
 module.exports = router
